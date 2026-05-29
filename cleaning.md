e49b6305ce158149788754d7fb343c59bb5eeffd
cactus-commits <aurabyte.dev@gmail.com>
2026-05-29 11:56:12 +0000

    feat: drop invalid rows from silver layer

--- cleaning.md
+++ cleaning.md
@@ -5,4 +5,17 @@
     - missing or invalid unit (`None`, `m`, `x`, empty, `6:40`, `07:35`, `71.5`): ~1,100 rows total
     -  d (days): 12 190 rows - dropped per instructions
 
-- `Event number of finishers`: drop 10 rows for "Ultra trail Dinarides - King's Race (CRO)" — all rows show 0 finishers, likely cancelled or incorrectly registered event
+- `Event number of finishers`: drop 10 rows for "Ultra trail Dinarides - King's Race (CRO)" — all rows show 0 finishers, likely cancelled or incorrectly registered event.
+
+- `Athlete age validation`
+    - Rows where `year_of_event - year_of_birth` < 10 or > 100: 284 rows.
+
+- `Athlete gender`
+    -  7 null rows → drop
+    -  46 rows with value `X` → drop (possibly non-binary registration, but cannot be confirmed).
+
+- `Athlete country & performance`
+    - `Athlete country`: 3 nulls
+    - `Athlete performance`: 2 nulls
+
+
--- transformations/silver/silver_obt.py
+++ transformations/silver/silver_obt.py
@@ -1,4 +1,41 @@
-import re
-from pyspark.sql.functions import col, to_timestamp,to_date, when
-from utils import utils
+from pyspark import pipelines as pd
+from utilities.utils import rename_columns_to_snackecase
+from pyspark.sql.functions import col, to_timestamp,to_date, when, round as spark_round
 
+
+
+# slå ihop till en table 
+@dp.table(
+    name="marathos.silver.marathos_obt",
+    comment="Cleaned historic race data and new race data for Marathos",
+    table_properties={
+        "delta.columnMapping.mode": "name",
+        "delta.minReaderVersion": "2",
+        "delta.minWriterVersion": "5"
+    },
+)
+
+def silver_obt():
+    df_historic = spark.sql("FROM STREAM marathos.bronze.um_races_raw")
+    df_new = spark.sql("FROM STREAM marathos.bronze.new_race_data")
+    
+    # Slå ihop till en tabell
+    df = df_historic.union(df_new)
+
+    # 1.Rename to snackecase
+    df = rename_columns_to_snackecase(df)
+    
+    # 2. Drop rows
+    df = df.filter(
+    col("event_distance_length").isNotNull() &     # nulls
+    ~col("event_distance_length").rlike("d$") &    # unit is days(d)
+    ~(col("event_distance_length") == "m") &     # unit is m
+    ~col("event_distance_length").rlike("x$") &    # unit is x
+    ~col("event_distance_length").rlike("^[0-9:.]+$") # no unit in string
+) 
+  
+
+
+    
+    # Kör samma cleaning på båda
+    return clean_races(df)
